0baaf69fad9310d4b315876041bef55f94e287d7
aiyuyun2015 <alvin2009@foxmail.com>
2021-12-29 10:14:18 +0800

    add event for ipc

---
+++ Problem_4/ThreadIPC.py
@@ -0,0 +1,35 @@
+from threading import *
+import time
+event = None
+def traffic_police():
+    while True:
+        time.sleep(5)
+        print("Traffic Police Giving GREEN Signal")
+        event.set()
+        time.sleep(10)
+        print("Traffic Police Giving RED Signal")
+        event.clear()
+
+def driver():
+    num=0
+    while True:
+        print("Drivers waiting for GREEN Signal")
+        event.wait()
+        print("Traffic Signal is GREEN...Vehicles can move")
+        while event.isSet():
+            num=num+1
+            print("Vehicle No:", num," Crossing the Signal")
+            time.sleep(2)
+        print("Traffic Signal is RED...Drivers have to wait")
+
+def test_event():
+    global event
+    event=Event()
+    t1=Thread(target=traffic_police)
+    t2=Thread(target=driver)
+    t1.start()
+    t2.start()
+
+
+if __name__ == '__main__':
+    test_event()
